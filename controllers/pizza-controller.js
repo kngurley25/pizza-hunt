52e0cfa8c8ff888b2d1a5db7c74c37cd52f494d6
kngurley <kngurley25@gmail.com>
2022-01-14 14:57:30 -0600

    add crud methods to pizza controller

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -23,6 +23,36 @@ const pizzaController = {
             console.log(err);
             res.status(400).json(err);
         })
+    },
+
+    createPizza({ body }, res) {
+        Pizza.create(body)
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
     }
 };
 
